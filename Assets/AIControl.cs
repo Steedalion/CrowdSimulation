ea5791a2521d7a0f31c5f5eef2b3e0c280d65d5f
(대학원생 기계공학과(공과대학)) CLINT ALEX STEED <steed@unist.ac.kr>
2021-04-30 17:13:21 +0900

    Fleeing not really working.

--- Assets/AIControl.cs
+++ Assets/AIControl.cs
@@ -1,17 +1,25 @@
 ﻿using UnityEngine;
+using UnityEngine.AI;
 
-public class AIControl : MonoBehaviour, IGoTo
+public class AIControl : MonoBehaviour, IGoTo, IFlee
 {
-    GameObject[] goalLocations;
-    UnityEngine.AI.NavMeshAgent agent;
+    public GameObject[] goalLocations;
+public        Vector3 currentGoal;
+
+    NavMeshAgent agent;
     Animator anim;
+    private static readonly int IsWalking = Animator.StringToHash("isWalking");
+    private float fleeRadius = 10;
+    private static readonly int IsRunning = Animator.StringToHash("isRunning");
+    private float detectionRadius = 5;
 
 
     // Use this for initialization
     void Start()
     {
         goalLocations = GameObject.FindGameObjectsWithTag("goal");
-        agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
         GoToDestination();
     }
 
@@ -19,14 +27,64 @@ public class AIControl : MonoBehaviour, IGoTo
     {
         if (agent.remainingDistance < 1)
         {
-            agent.SetDestination(goalLocations[Random.Range(0, goalLocations.Length)].transform.position);
+            GoToDestination();
+            Walk();
         }
     }
 
     public void GoToDestination()
     {
-        agent.SetDestination(goalLocations[Random.Range(0, goalLocations.Length)].transform.position);
-        anim = this.GetComponent<Animator>();
-        anim.SetTrigger("isWalking");
+        currentGoal = goalLocations[Random.Range(0, goalLocations.Length)].transform.position;
+        agent.SetDestination(currentGoal);
+        Debug.Log( "Going to"+currentGoal);
     }
+
+    public void DetectDanger(Vector3 dangerPosition)
+    {
+        Debug.Log("informed");
+        FleeFromDanger(dangerPosition);
+    }
+
+    private void FleeFromDanger(Vector3 dangerPosition)
+    {
+        Vector3 awayFromDanger = (transform.position - dangerPosition).normalized;
+        if (awayFromDanger.magnitude < detectionRadius)
+        {
+            Vector3 awayFromDangerDirection = (transform.position - dangerPosition).normalized;
+            Vector3 goal = transform.position + awayFromDangerDirection * fleeRadius;
+            Vector3 validGoal = GetAValidGoal(goal);
+            RunTo(validGoal);
+        }
+    }
+
+    private Vector3 GetAValidGoal(Vector3 goal)
+    {
+        NavMeshPath path = new NavMeshPath();
+        agent.CalculatePath(goal, path);
+        Vector3 finalPointInPath = path.corners[path.corners.Length - 1];
+        return finalPointInPath;
+    }
+
+    private void RunTo(Vector3 validGoal)
+    {
+        float runSpeed = 5;
+        float runTurnSpeed = 999;
+        anim.SetTrigger(IsRunning);
+        anim.speed = runSpeed;
+        agent.angularSpeed = runTurnSpeed;
+    }
+
+    private void Walk()
+    {
+        float walkSpeed = 2;
+        float walkTurnSpeed = 12;
+        anim.SetTrigger(IsWalking);
+        anim.speed = walkSpeed;
+        agent.angularSpeed = walkTurnSpeed;
+    }
+}
+
+public interface IFlee
+{
+    void DetectDanger(Vector3 dangerPosition);
 }
---
+++ Assets/DropOnClick.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class DropOnClick : MonoBehaviour
+{
+    public GameObject prefab;
+    public IFlee[] fleeingAgents;
+
+    private void Start()
+    {
+        fleeingAgents = FindObjectsOfType<MonoBehaviour>().OfType<IFlee>().ToArray();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SpawnAtClickPosition();
+        }
+    }
+
+    void SpawnAtClickPosition()
+    {
+        Vector3 spawnPosition = Input.mousePosition;
+        Ray mouseRay = Camera.main.ScreenPointToRay(spawnPosition);
+
+        if (Physics.Raycast(mouseRay, out RaycastHit raycastHit))
+        {
+            InstantiateObject(raycastHit.point);
+            InformAgents(raycastHit.point);
+        }
+    }
+
+    private void InformAgents(Vector3 dangerPosition)
+    {
+        foreach (IFlee flee in fleeingAgents)
+        {
+            flee.DetectDanger(dangerPosition);
+        }
+    }
+
+    private void InstantiateObject(Vector3 intersection)
+    {
+        Instantiate(prefab, intersection, Quaternion.identity);
+    }
+}
