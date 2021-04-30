a8e07420a1d4f1ef3b181532348776c400b1f389
(대학원생 기계공학과(공과대학)) CLINT ALEX STEED <steed@unist.ac.kr>
2021-04-30 14:17:37 +0900

    Flow navmesh agents scene done.

---
+++ Assets/GoTo.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class GoTo : MonoBehaviour
+{
+    public Transform destination;
+    private NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(destination.position);
+    }
+
+}
